ba3d741d703c7948e0e986446f3f11def3d15ca1
Rama Vats <guy55222@gmail.com>
2023-04-06 23:50:48 +0530

    added config

--- firebase.js
+++ firebase.js
@@ -3,7 +3,13 @@ import 'firebase/auth'
 import 'firebase/firestore'
 
 const config = {
-  // Replace with your own config object from Firebase
+    apiKey: "AIzaSyBnb-KMTu-PDNbThVdm_7tab5ad6w7UVZk",
+    authDomain: "my-cat-app-19908.firebaseapp.com",
+    projectId: "my-cat-app-19908",
+    storageBucket: "my-cat-app-19908.appspot.com",
+    messagingSenderId: "621222619825",
+    appId: "1:621222619825:web:e5850625b4fa27d0319090",
+    measurementId: "G-EQPH44FFJY"
 }
 
 if (!firebase.apps.length) {
